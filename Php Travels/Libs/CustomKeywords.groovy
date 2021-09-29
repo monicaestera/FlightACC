06e82e94ecb0ca5d5171c2bfb5d12d40c2abdf57
monicaestera <monica.estera@ti.ukdw.ac.id>
2021-09-29 09:26:09 +0700

    Add files via upload
    Change-Id: I72944ab0ec2b87e4787f62dd99dba9c49d3d2b1f
    Signed-off-by: monicaestera <monica.estera@ti.ukdw.ac.id>

--- Php Travels/Libs/CustomKeywords.groovy
+++ Php Travels/Libs/CustomKeywords.groovy
@@ -3,24 +3,20 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
-import com.applitools.eyes.selenium.Eyes
-
-import org.openqa.selenium.WebElement
+import java.lang.String
 
 import com.kms.katalon.core.testobject.TestObject
 
-import java.lang.String
-
 import com.applitools.eyes.RectangleSize
 
+import com.applitools.eyes.selenium.Eyes
 
 
-def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkElement"(
-    	Eyes eyes	
-     , 	WebElement element	) {
-    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkElement(
-        	eyes
-         , 	element)
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkWindow"(
+    	String testName	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkWindow(
+        	testName)
 }
 
 
@@ -33,10 +29,8 @@ def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkTestObject"(
 }
 
 
-def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkWindow"(
-    	String testName	) {
-    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkWindow(
-        	testName)
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesInit"() {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesInit()
 }
 
 
@@ -53,11 +47,6 @@ def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesClose"(
     	Eyes eyes	) {
     (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesClose(
         	eyes)
-}
-
-
-def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesInit"() {
-    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesInit()
 }
 
 
